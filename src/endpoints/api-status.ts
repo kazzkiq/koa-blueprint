2b9069bbde706278e18cefdf5342fcbbe8d568bd
Claudio Holanda Jr <claudiohw@gmail.com>
2023-04-30 16:04:06 -0300

    feat: removing irrelevant packages

--- src/endpoints/api-status.ts
+++ src/endpoints/api-status.ts
@@ -7,21 +7,21 @@ import Router from "@koa/router";
 const router = new Router();
 
 router.get("/", (ctx: Koa.BaseContext, next: Koa.Next) => {
-  /**
-   * Endpoint documentation:
-   *
-   * #swagger.tags = ['Status']
-   * #swagger.responses[200] = {
-      description: 'User successfully obtained.',
-      schema: {
-        status: "API working.",
-        server_date: "2023-04-30T06:35:51.766Z",
-        cores: 6,
-        cluster_id: 1,
-      }
-    }
-   */
+  // Endpoint documentation:
+  //
+  // #swagger.tags = ['App']
+  // #swagger.responses[200] = {
+  //   schema: {
+  //      status: "API working.",
+  //      server_date: "2023-04-30T06:35:51.766Z",
+  //      cores: 6,
+  //      cluster_id: 1,
+  //    }
+  //  }
+  //
+  // (end swagger docs)
 
+  ctx.status = 200;
   ctx.body = {
     status: "API working.",
     server_date: new Date(),
